1a4556cf65c139e4869294fd8c195688ed464665
anvishacc <108969287+anvishacc@users.noreply.github.com>
2022-09-08 04:06:11 +1000

    Add files via upload

---
+++ SIT 120 Weekly tasks/Assessment-SIt-120-Anvisha-s221536401/All tasks and assessments SIT 120/week 1 tasks for SIT 120/todo.js
@@ -0,0 +1,11 @@
+let vue=new Vue({
+    el: "#app", 
+    data: {
+        todos: [
+            {title : 'Assignment 1',checked:true},
+            {title : 'Assignment 2',checked:false},
+            {title : 'Assignment 3',checked:false
+        },
+        ],
+    },
+});
